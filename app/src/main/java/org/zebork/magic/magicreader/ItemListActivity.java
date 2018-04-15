718125d79d1456cc63d691344cc9fc37f1b1400f
Jeryter <wzhi2013@gmail.com>
2018-04-15 20:58:36 +0800

    Update ItemListActivity.java
    Add IMSI, IMEI and MAC.

--- app/src/main/java/org/zebork/magic/magicreader/ItemListActivity.java
+++ app/src/main/java/org/zebork/magic/magicreader/ItemListActivity.java
@@ -1,19 +1,26 @@
 package org.zebork.magic.magicreader;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.telephony.TelephonyManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.app.ActivityManager;
+import android.widget.Toast;
 
 import org.zebork.magic.magicreader.dummy.DummyContent;
 
@@ -27,6 +34,7 @@ import java.util.List;
  * lead to a {@link ItemDetailActivity} representing
  * item details. On tablets, the activity presents the list of items and
  * item details side-by-side using two vertical panes.
+ * 主界面
  */
 public class ItemListActivity extends AppCompatActivity {
 
@@ -74,7 +82,62 @@ public class ItemListActivity extends AppCompatActivity {
         View recyclerView = findViewById(R.id.item_list);
         assert recyclerView != null;
         setupRecyclerView((RecyclerView) recyclerView);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.READ_PHONE_STATE}, 1);
+        }
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    DummyContent.ITEM_MAP.get("1").details += getInfo();
+                } else {
+                    Toast.makeText(this, "权限拒绝！", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+        }
+    }
+
+    /**
+     * 获取IMEI号，IESI号，手机型号
+     */
+    private String getInfo() {
+        String imi = null;
+        try {
+            TelephonyManager mTm = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+            String imei = mTm.getDeviceId();
+            String imsi = mTm.getSubscriberId();
+            String mtype = android.os.Build.MODEL; // 手机型号
+            String mtyb = android.os.Build.BRAND;//手机品牌
+            String numer = mTm.getLine1Number(); // 手机号码，有的可得，有的不可得
+            imi = "手机IMEI号：" + imei + "\n手机IESI号：" + imsi + "\n手机型号：" + mtype + "\n手机品牌：" + mtyb + "\n手机号码" + numer + "\n";
+        } catch (SecurityException e) {
+            imi = "";
+        }
+        String macAddr = "";
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        macAddr = "手机macAdd:" + wifiInfo.getMacAddress();
+
+        return imi + macAddr + "\n";
+    }
+
+    /**
+     * 获取手机MAC地址
+     * 只有手机开启wifi才能获取到mac地址
+
+    private String getMacAddress(){
+        String macAddr = "";
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        macAddr = wifiInfo.getMacAddress();
+        return "手机macAdd:" + macAddr;
+    }*/
+
 
     private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
         recyclerView.setAdapter(new SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, mTwoPane));
