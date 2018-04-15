e42e859bc7a370433859c7a64fa7a2d96b4bd587
Jeryter <wzhi2013@gmail.com>
2018-04-15 23:54:50 +0800

    Add IMSI, IMEI, etc.

--- app/src/main/java/org/zebork/magic/magicreader/dummy/InfoGetter.java
+++ app/src/main/java/org/zebork/magic/magicreader/dummy/InfoGetter.java
@@ -41,10 +41,25 @@ public class InfoGetter {
             TelephonyManager mTm = (TelephonyManager) ctx.getSystemService(ctx.TELEPHONY_SERVICE);
             String imei = mTm.getDeviceId();
             String imsi = mTm.getSubscriberId();
-            String mtype = android.os.Build.MODEL; // 手机型号
-            String mtyb = android.os.Build.BRAND;//手机品牌
-            String numer = mTm.getLine1Number(); // 手机号码，有的可得，有的不可得
-            imi = "手机IMEI号：" + imei + "\n手机IESI号：" + imsi + "\n手机型号：" + mtype + "\n手机品牌：" + mtyb + "\n手机号码：" + numer + "\n";
+            String mtype = android.os.Build.MODEL;      // 手机型号
+            String mtyb = android.os.Build.BRAND;       // 手机品牌
+            String numer = mTm.getLine1Number();        // 手机号码，有的可得，有的不可得
+            String sim = mTm.getSimSerialNumber();      // SIM卡的ICCID
+            String imsi_o = mTm.getSubscriberId();        // 运营商
+            if (imsi_o != null) {
+                if (imsi.startsWith("46000") || imsi.startsWith("46002") || imsi.startsWith("46007")) {
+                    // 因为移动网络编号46000下的IMSI已经用完，所以虚拟了一个46002编号，134/159号段使用了此编号
+                    imsi_o = "中国移动";
+                } else if (imsi.startsWith("46001") || imsi.startsWith("46006")) {
+                    imsi_o = "中国联通";
+                } else if (imsi.startsWith("46003") || imsi.startsWith("46005") || imsi.startsWith("46011")) {
+                    imsi_o = "中国电信";
+                } else
+                    imsi_o = imsi_o.substring(0, 5);
+            }
+            imi = "手机IMEI号：" + imei + "\n手机IESI号：" + imsi + "\n手机型号：" + mtype
+                    + "\n手机品牌：" + mtyb + "\n手机号码：" + numer + "\nSIM ICCID：" + sim
+                    + "\n运营商：" + imsi_o;
         } catch (SecurityException e) {
             imi = "";
         }
